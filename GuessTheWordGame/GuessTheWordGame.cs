1d8533afb982b9e9f01f2255ba10f571b9014456
V.Piletskiy <v.piletskyi@lampalampa.net>
2022-11-02 15:04:32 +0200

    guess the word game finished

---
+++ GuessTheWordGame/GuessTheWordGame.cs
@@ -0,0 +1,75 @@
+﻿namespace GuessTheWordGame;
+
+public class GuessTheWordGame
+{
+    private char[] word;
+    private char[] userGuess;
+
+    private int triesCount;
+    
+    private void DrawWord()
+    {
+        Console.WriteLine($"Word you need to guess has {userGuess.Length} letters.");
+        Console.WriteLine(string.Join("", userGuess));
+        Console.WriteLine("");
+    }
+
+    public void StartGame()
+    {
+        string[] fileWithWords =
+            File.ReadAllLines($"../../../Resources/WordsStock.txt");
+
+        Random random = new Random();
+
+        int indexOfWord = random.Next(0, fileWithWords.Length);
+
+        word = fileWithWords[indexOfWord].ToCharArray();
+
+        userGuess = new char[word.Length];
+        triesCount = word.Length * 5;
+
+        for (var i = 0; i < word.Length; i++)
+        {
+            userGuess[i] = '-';
+        }
+
+        // game loop
+
+        bool gameWon = false,
+            gameFinished = false;
+
+        while (!gameFinished) 
+        {
+            Console.WriteLine($"You have {triesCount} tries left");
+
+            Console.Write("Enter your symbol you guess is in word: ");
+
+            char symbol = char.Parse(Console.ReadLine() ?? string.Empty);
+
+            CheckIfSymbolIsInWord(symbol);
+
+            gameWon = CheckIfGameWon();
+
+            DrawWord();
+
+            triesCount--;
+
+            gameFinished = triesCount == 0 || gameWon;
+        }
+
+        Console.WriteLine(gameWon ? "You have won" : $"You loose, the word you needed to guess was {string.Join("", word)}");
+    }
+
+    private bool CheckIfGameWon()
+    {
+        return userGuess.Contains('-') == false;
+    }
+
+    private void CheckIfSymbolIsInWord(char symbol)
+    {
+        for (var i = 0; i < word.Length; i++)
+        {
+            if (word[i] == symbol) userGuess[i] = symbol;
+        }
+    }
+}
--- GuessTheWordGame/Program.cs
+++ GuessTheWordGame/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace GuessTheWordGame // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessTheWordGame guessTheWordGame = new GuessTheWordGame();
+            
+            guessTheWordGame.StartGame();
+        }
+    }
+}
